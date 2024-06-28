fc7c41e088295bda35d36467ebcf6cbb876aa87f
Ezekiel <realnyamasege2@gmail.com>
2024-06-28 14:20:27 +0300

    done

--- server/app.py
+++ server/app.py
@@ -20,6 +20,60 @@ def index():
     body = {'message': 'Welcome to the pet directory!'}
     return make_response(body, 200)
 
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        body = pet.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Pet {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = []  # array to store a dictionary for each pet
+    for pet in Pet.query.filter_by(species=species).all():
+        pets.append(pet.to_dict())
+    body = {'count': len(pets),
+            'pets': pets
+            }
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+#!/usr/bin/env python3
+#server/seed.py
+from random import choice as rc
+from faker import Faker
+
+from app import app
+from models import db, Pet
+
+with app.app_context():
+
+    # Create and initialize a faker generator
+    fake = Faker()
+
+    # Delete all rows in the "pets" table
+    Pet.query.delete()
+
+    # Create an empty list
+    pets = []
+
+    species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+
+    # Add some Pet instances to the list
+    for n in range(10):
+        pet = Pet(name=fake.first_name(), species=rc(species))
+        pets.append(pet)
+
+    # Insert each Pet in the list into the "pets" table
+    db.session.add_all(pets)
+
+    # Commit the transaction
+    db.session.commit()
